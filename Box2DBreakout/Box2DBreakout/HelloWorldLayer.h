b1721fc9b152a4044efee38f1642ca989852b042
Marryat Stevens <marryat@marryat.net>
2012-05-10 20:32:03 +0100

    Added in declarations to create ball Definition -> Body Object -> Shape -. Fixture Definition -> Fixture Object in turn.

--- Box2DBreakout/Box2DBreakout/HelloWorldLayer.h
+++ Box2DBreakout/Box2DBreakout/HelloWorldLayer.h
@@ -7,8 +7,13 @@
 //
 
 #import "cocos2d.h"
+#import "Box2D.h"
 
 @interface HelloWorldLayer : CCLayer {   
+    b2World *_world;
+    b2Body *_groundBody;
+    b2Fixture *_bottomFixture;
+    b2Fixture *_ballFixture;
 }
 
 + (id) scene;
--- Box2DBreakout/Box2DBreakout/HelloWorldLayer.mm
+++ Box2DBreakout/Box2DBreakout/HelloWorldLayer.mm
@@ -10,6 +10,8 @@
 // Import the interfaces
 #import "HelloWorldLayer.h"
 
+#define PTM_RATIO 32
+
 @implementation HelloWorldLayer
 
 + (id)scene {
@@ -24,6 +26,53 @@
 - (id)init {
     
     if ((self=[super init])) {
+        CGSize winSize = [CCDirector sharedDirector].winSize;
+        
+        b2Vec2 gravity = b2Vec2(0.0f, 0.0f);
+        bool doSleep = true;
+        _world = new b2World(gravity, doSleep);
+        
+        //Create edges around the entire screen
+        b2BodyDef groundBodyDef;
+        groundBodyDef.position.Set(0,0);
+        _groundBody = _world->CreateBody(&groundBodyDef);
+        b2PolygonShape groundBox;
+        b2FixtureDef groundBoxDef;
+        groundBoxDef.shape = &groundBox;
+        groundBox.SetAsEdge(b2Vec2(0,0), b2Vec2(winSize.width/PTM_RATIO, 0));
+        _bottomFixture = _groundBody->CreateFixture(&groundBoxDef);
+        groundBox.SetAsEdge(b2Vec2(0, winSize.height/PTM_RATIO), b2Vec2(winSize.width/PTM_RATIO, winSize.height/PTM_RATIO));
+        _groundBody->CreateFixture(&groundBoxDef);
+        groundBox.SetAsEdge(b2Vec2(0,winSize.height/PTM_RATIO), b2Vec2(winSize.width/PTM_RATIO, winSize.height/PTM_RATIO));
+        _groundBody->CreateFixture(&groundBoxDef);
+        groundBox.SetAsEdge(b2Vec2(winSize.width/PTM_RATIO, winSize.height/PTM_RATIO), b2Vec2(winSize.width/PTM_RATIO, 0));
+        _groundBody->CreateFixture(&groundBoxDef);
+     
+        //Create the ball Sprite and add it to the layer
+        CCSprite *ball = [CCSprite spriteWithFile:@"Ball.jpg" rect:CGRectMake(0, 0, 52, 52)];
+        ball.position = ccp(100, 100);
+        ball.tag = 1;
+        [self addChild:ball];
+                          
+        //Create Ball Body
+        b2BodyDef ballBodyDef;
+        ballBodyDef.type = b2_dynamicBody;
+        ballBodyDef.position.Set(100/PTM_RATIO, 100/PTM_RATIO);
+        ballBodyDef.userData = ball;
+        b2Body * ballBody = _world->CreateBody(&ballBodyDef);
+        
+        //Create Circle Shape
+        b2CircleShape circle;
+        circle.m_radius = 26.0/PTM_RATIO;
+        
+        //Create shape definition and add to body
+        b2FixtureDef ballShapeDef;
+        ballShapeDef.shape = &circle;
+        ballShapeDef.density = 1.0f;
+        ballShapeDef.friction = 0.0f;
+        ballShapeDef.restitution = 1.0f;
+        _ballFixture = ballBody->CreateFixture(&ballShapeDef);
+        
     }
     return self;
 }
